6b5d3d1b89b7a5f0910a57f1eeb5b87d0635d291
Subham Rakshit <111345784+SubhamRakshit97@users.noreply.github.com>
2024-09-22 16:36:35 +0530

    Update urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -1,18 +1,9 @@
-# from django.urls import path
-# from .views import fetch_weather_and_store, weather_view, weather_trends_view
-
-# urlpatterns = [
-#     path('fetch/<str:city>/', fetch_weather_and_store, name='fetch_weather_and_store'),
-#     path('weather/<str:city>/', weather_view, name='weather_view'),
-#     path('trends/<str:city>/', weather_trends_view, name='weather_trends_view'),
-# ]
-
 from django.urls import path
-from .views import weather_view
+from .views import weather_view,fetch_weather_data
 
 app_name = 'weather'
 
 urlpatterns = [
     path('', weather_view, name='weather'),
+    path('fetch/', fetch_weather_data, name='fetch_weather_data')
 ]
-
